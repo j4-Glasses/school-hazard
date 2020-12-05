155cbbfcedd8ca758ab8c8df9a6e95c7d5efdfc5
Koyama-Yu <xiaoshanyu427@gmail.com>
2020-12-05 09:01:49 +0000

    盾の表示を目指してbattleとmainを編集したよ

--- code/main.rb
+++ code/main.rb
@@ -2,10 +2,19 @@ require "dxopal"
 include DXOpal
 
 require_remote 'battle.rb'
+Image.register(:shield, 'shield.png') 
+
 
 Window.load_resources do
-  mouse = Battle.new
+  shield_img = Image[:shield]
+  #shield_img.scale_x=(0.10)
+  #shield_img.scale_y=(0.10)
+  
+  mouse = Battle.new(shield_img)
   Window.loop do
     mouse.mouse
+    if mouse_down?(M_RBUTTON) #It doesn't work!
+      mouse.shield
+    end
   end
 end
