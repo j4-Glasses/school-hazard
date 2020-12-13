c2d85a7a63af19f0804b2f86e537b8331e627285
FujiKohei <sabanomisoni1228@icloud.com>
2020-12-13 08:47:08 +0000

    無駄なコメント文を消した

--- code/battle.rb
+++ code/battle.rb
@@ -1,9 +1,5 @@
 #戦闘のプログラム
 class Battle < Sprite
-    #def initialize(shield_img, gunshot_img)
-    #   @shield_img = shield_img
-    #   @gunshot_img = gunshot_img
-    #end
     def update
         @x = Input.mouse_x  # マウスカーソルのx座標
         @y = Input.mouse_y  # マウスカーソルのy座標
@@ -12,15 +8,12 @@ class Battle < Sprite
     end
 
     def shield(shield_img)
-        #Window.draw_scale(@x-48, @y-48, @shield_img, 0.10, 0.10, 0, 0) 
         scaleAmount = 0.10
         Window.draw_scale(@x - (shield_img.width * scaleAmount / 2),@y - (shield_img.height * scaleAmount / 2), shield_img, scaleAmount, scaleAmount, 0, 0) 
-        #Sprite.draw(@x, @y, @shield_img)
     end
     
     def attack(gunshot_img)
         scaleAmount = 0.20
         Window.draw_scale(@x - (gunshot_img.width * scaleAmount / 2) + 6, @y - (gunshot_img.height * scaleAmount / 2), gunshot_img, scaleAmount, scaleAmount, 0, 0) 
-        #Window.draw_scale(@x , @y , gunshot_img, 0.20, 0.20, 0, 0) 
     end
 end
