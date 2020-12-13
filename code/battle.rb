266c46be4a915335fd32e133d13806ca1d228686
Koyama-Yu <xiaoshanyu427@gmail.com>
2020-12-13 01:45:39 +0000

    盾と銃痕の表示

--- code/battle.rb
+++ code/battle.rb
@@ -1,8 +1,9 @@
 #戦闘のプログラム
 class Battle
-    def initialize(shield_img)
-        @shield_img = shield_img
-    end
+    #def initialize(shield_img, gunshot_img)
+     #   @shield_img = shield_img
+      #  @gunshot_img = gunshot_img
+    #end
     def mouse
         @x = Input.mouse_x  # マウスカーソルのx座標
         @y = Input.mouse_y  # マウスカーソルのy座標
@@ -10,8 +11,16 @@ class Battle
         Window.draw_circle(@x, @y, @r, C_RED, 0)
     end
 
-    def shield
-        Window.draw_scale(@x-48, @y-48, @shield_img, 0.10, 0.10, 0, 0) 
+    def shield(shield_img)
+        #Window.draw_scale(@x-48, @y-48, @shield_img, 0.10, 0.10, 0, 0) 
+        scaleAmount = 0.10
+        Window.draw_scale(@x - (shield_img.width * scaleAmount / 2), @y - (shield_img.height * scaleAmount / 2), shield_img, scaleAmount, scaleAmount, 0, 0) 
         #Sprite.draw(@x, @y, @shield_img)
+    end
+    
+    def attack(gunshot_img)
+        scaleAmount = 0.20
+        Window.draw_scale(@x - (gunshot_img.width * scaleAmount / 2) + 6, @y - (gunshot_img.height * scaleAmount / 2), gunshot_img, scaleAmount, scaleAmount, 0, 0) 
+        #Window.draw_scale(@x , @y , gunshot_img, 0.20, 0.20, 0, 0) 
     end
 end
--- main.rb
+++ main.rb
@@ -3,21 +3,26 @@ include DXOpal
 
 require_remote './code/battle.rb'
 Image.register(:shield, './images/shield.png')
+Image.register(:gunshot, './images/gunshot_wound.png')
 
 
 
 Window.load_resources do
   Window.height=600
   Window.width=600
+  Window.bgcolor=C_BLUE
   shield_img = Image[:shield]
+  gunshot_img = Image[:gunshot]
   #shield_img.scale_x=(0.10)
   #shield_img.scale_y=(0.10)
 
-  mouse = Battle.new(shield_img)
+  mouse = Battle.new(shield_img, gunshot_img)
   Window.loop do
     mouse.mouse
     if Input.mouse_down?(M_RBUTTON) == true
-      mouse.shield
+      mouse.shield(shield_img)
+    elsif Input.mouse_down?(M_LBUTTON) == true
+      mouse.attack(gunshot_img)
     end
   end
 end
