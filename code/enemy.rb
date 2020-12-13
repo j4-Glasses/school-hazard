0a8e27bf5db6018036e0a328646bf7b36583c7f9
FujiKohei <sabanomisoni1228@icloud.com>
2020-12-13 02:57:19 +0000

    変数をついか

---
+++ code/enemy.rb
@@ -0,0 +1,19 @@
+class Enemy < Sprite
+    def intitialize(img,hp,pattern)
+        @img = img
+        @hp = hp
+        @pattern = pattern
+    end
+    def move
+        case @pattern
+        when 0
+            
+        end
+    end
+    def hit
+        
+    end
+    def statas
+        
+    end
+end
--- main.rb
+++ main.rb
@@ -2,21 +2,29 @@ require "dxopal"
 include DXOpal
 
 require_remote './code/battle.rb'
+require_remote './code/enemy.rb'
 Image.register(:shield, './images/shield.png')
 Image.register(:gunshot, './images/gunshot_wound.png')
+Image.register(:ball, './images/ball.png')
+Image.register(:hara, './images/hara.png')
 
-
+WINDOW_HEIGHT = 600
+WINDOW_WIDTH = 600
 
 Window.load_resources do
-  Window.height=600
-  Window.width=600
+  Window.height = WINDOW_HEIGHT
+  Window.width = WINDOW_WIDTH
   Window.bgcolor=C_BLUE
   shield_img = Image[:shield]
   gunshot_img = Image[:gunshot]
-  #shield_img.scale_x=(0.10)
-  #shield_img.scale_y=(0.10)
+  ball_img = Image[:ball]
+  hara_img = Image[:hara]
 
   mouse = Battle.new(shield_img, gunshot_img)
+  enemies = []
+  enemies[0] = Enemy.new(ball_img,100,0)
+  enemies[1] = Enemy.new(hara_img,300,2)
+  
   Window.loop do
     mouse.mouse
     if Input.mouse_down?(M_RBUTTON) == true
